41637adfde8929678879c7397c918fc16ae659f1
Yandile-Matyityilili <matyityililiy@gmail.com>
2025-02-25 16:23:53 +0200

    to be fixed

---
+++ frontend/src/router/index.js
@@ -0,0 +1,78 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import SignupView from '../views/SignupView.vue'
+import SigninView from '@/views/SigninView.vue';
+//import Checkout from '../views/CheckoutView.vue';
+import ReadyMadeMeals from '@/views/ReadyMadeMeals.vue'
+import MealKitsView from '../views/MealKitsView.vue'
+import CartView from '../views/CartView.vue'
+import DeliveryView from '../views/DeliveryView.vue'
+import SupplierView from '../views/SupplierView.vue'
+import ClientDeliInterfaceView from '@/views/ClientDeliInterfaceView.vue';
+
+const routes = [
+  {
+    path: '/home',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/',
+    name: 'SignupPage',
+    component: SignupView
+  },
+  {
+    path: '/SigninPage',
+    name: 'SigninPage',
+    component: SigninView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  //{
+   // path:'/checkout',
+    //component:CheckoutView
+  //},
+  {
+    path: '/ready-made-meals',
+    component: ReadyMadeMeals
+  },
+  {
+    path:'/meal-kits',
+    component: MealKitsView 
+  },
+  {
+    path:'/cart',
+    component: CartView
+  },
+  {
+    path: '/delivery',
+    name: 'delivery',
+    component: DeliveryView
+  },
+  {
+    path:'/supplier',
+    name:'supplier',
+    component: SupplierView
+  },
+  {
+    path: '/client-delivery-interface',
+    name: 'client-delivery-interface',
+    component: ClientDeliInterfaceView
+  }
+]
+    
+
+const router = createRouter({
+  history: createWebHashHistory(process.env.BASE_URL),
+  routes,
+});
+
+
+export default router
+
